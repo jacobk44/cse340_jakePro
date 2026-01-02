cb957cd0b66621cbaa735fadbef6d3f8f8aa769d
jacobk44 <jacobkwartengamoah@gmail.com>
2026-01-02 02:05:51 -0800

    added code

--- server.js
+++ server.js
@@ -2,25 +2,42 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
+const path = require("path")
+
 const app = express()
-const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+
+/* ***********************
+ * Static Files
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
